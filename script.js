425d24cd68c98f1d78e9560d2e783abca34d6445
Gowthami <gowthami09nov92@gmail.com>
2025-06-14 12:34:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//variables
+/*
+let
+const
+var
+*/
+
+const x = 100; //x is stored as constant
+document.write(x+"<br>");
+let name = "Harish"
+document.write(name)
+document.write("<br>")
+var Name = "Girish"
+document.write(Name)
+//array - same data
+//data - numeric, string, array, object
+
+
